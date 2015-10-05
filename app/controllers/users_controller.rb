2f16308ccf71e64e97ff3a6feeb3c87f5c428261
noritakamishima <noritaka.mishima@gmail.com>
2015-10-05 10:46:52 +0000

    add user model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+            flash[:success] = "Welcome to the Sample App!"
         redirect_to @user # ここを修正
     else
       render 'new'
@@ -23,7 +24,7 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :email, :password,
-                                 :password_confirmation)
+                                 :password_confirmation,:area,:profile)
   end
   
   
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151003074332) do
+ActiveRecord::Schema.define(version: 20151005062124) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 20151003074332) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "profile"
+    t.string   "area"
   end
 
 end
