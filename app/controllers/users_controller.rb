36895ef7fc9de90336d91caea2604a5302363f11
noritakamishima <noritaka.mishima@gmail.com>
2015-10-06 05:16:28 +0000

    add posting microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,9 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :set_user, only: [:edit, :update, :destroy]
   
   def show # 追加
+      @user = User.find(params[:id])
+    @microposts = @user.microposts
   end
   
   def new
